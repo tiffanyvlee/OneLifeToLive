35f00f0965ca72ed412bbf94fb3b7a6c145ff20f
yingtingyang <yangy389@newschool.edu>
2016-10-29 21:32:17 -0400

    jump\effect

---
+++ OneLifeToLive/Assets/soundeffect.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class soundeffect : MonoBehaviour {
+    public AudioSource jump;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<AudioSource>();
+            jump.Play();
+        }
+	}
+}
